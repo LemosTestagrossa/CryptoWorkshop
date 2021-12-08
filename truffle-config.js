7509bc78be3b39b7e9cba5b1e5de2585f07910a0
Franco Testagrossa <franco.testagrossa@gmail.com>
2021-12-08 02:28:26 +0100

    Added chai

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,25 @@
 const path = require("path");
 
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+
 module.exports = {
-// See <http://truffleframework.com/docs/advanced/configuration>
-// to customize your Truffle configuration!
-contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+
 networks: {
     development: {
-    port: 7545,
-    network_id: "*",
-    host: "127.0.0.1"
-    }
+        port: 8545,
+        network_id: "*",
+        host: "127.0.0.1"
+    },
+    testnet: {
+        provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+        network_id: 97,
+        confirmations: 10,
+        timeoutBlocks: 200,
+        skipDryRun: true
+      },
 },
 compilers: {
     solc: {
