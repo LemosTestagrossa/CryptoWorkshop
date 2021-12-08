c5e2e9afdf01eedd30074d614b4f6cc91215953e
Franco Testagrossa <franco.testagrossa@gmail.com>
2021-12-08 03:14:49 +0100

    Extract token constructor parameters

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,7 +4,11 @@ pragma solidity >0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("StarDucks Capu-Token", "SCT") {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
     }
 }
